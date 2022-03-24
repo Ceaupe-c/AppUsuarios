610e9dd4305b9c6bd431470612994d320357606b
Ceaupe-c <cesaraugusto7817@gmail.com>
2022-03-24 13:46:52 -0500

    Directrices vueJS

---
+++ src/components/Test.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+    <div v-text="title"></div>
+    <div v-if="showName">
+    <p>{{user.firstName}}</p>
+    </div>
+    <div v-else>
+        <p>No puedo mostrar nada mi pez</p>
+    </div>
+    <hr>
+    <ul>
+        <li v-for="task in tasks">
+        {{task.title}}
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+   data(){
+       return {
+           title: "Hola CumJS",
+           user: {
+               firstName: "Julio",
+               lastName: "Casas"
+           },
+           myHtml: "<h2>Hola vueuwu</h2>",
+           showName: false,
+           tasks: [
+               {title: 'eat'},
+               {title: 'dinner'},
+               {title: 'lunch'}
+           ]
+       }
+   }
+}
+</script>
+
+<style media="screen">
+
+</style>
